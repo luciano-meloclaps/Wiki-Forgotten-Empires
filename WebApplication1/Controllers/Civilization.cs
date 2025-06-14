b36886398503108f8ee20c9dace64fc525c2e72e
Luciano <meloclapsluciano@gmail.com>
2025-06-14 18:43:44 -0300

    Initial commit

---
+++ WebApplication1/Controllers/Civilization.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WikiApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Civilization : ControllerBase
+    {
+    }
+}
