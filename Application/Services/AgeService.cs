974a1207efc8d2b372923bb2ad7032c4f83ea7cd
Luciano <meloclapsluciano@gmail.com>
2025-07-15 20:49:15 -0300

    Aplico .gitignore: excluyo bin/, obj/ y demás artefactos

---
+++ Application/Services/AgeService.cs
@@ -0,0 +1,46 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class AgeService : IAgeService
+    {
+        private readonly IAgeRepository _ageRepository;
+        public AgeService(IAgeRepository ageRepository)
+        {
+            _ageRepository = ageRepository ?? throw new ArgumentNullException(nameof(ageRepository));
+        }
+
+        public async Task<IEnumerable<Age>> GetAllAges()
+        {
+            return await _ageRepository.GetAllAges();
+        }
+        public async Task<Age> CreateAge(Age age)
+        {
+            if (age == null) throw new ArgumentNullException(nameof(age));
+            return await _ageRepository.CreateAge(age);
+        }
+        public async Task<Age> GetAgeById(int id)
+        {
+            return await _ageRepository.GetAgeById(id);
+        }
+        public async Task<bool> UpdateAge(int id, Age age)
+        {
+            if (age == null) throw new ArgumentNullException(nameof(age));
+            return await _ageRepository.UpdateAge(id, age);
+        }
+        public async Task<bool> DeleteAge(int id)
+        {
+            return await _ageRepository.DeleteAge(id);
+        }
+
+    }
+        
+}
---
+++ WebApplication1/Controllers/AgeController.cs
@@ -0,0 +1,59 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForgottenEmpire.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgeController : ControllerBase
+    {
+        private readonly IAgeService _ageService;
+        public AgeController(IAgeService ageService)
+        {
+            _ageService = ageService ?? throw new ArgumentNullException(nameof(ageService));
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllAges()
+        {
+            var ages = await _ageService.GetAllAges();
+            return Ok(ages);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAge([FromBody] Age age)
+        {
+            if (age == null)
+            {
+                return BadRequest("Age cannot be null");
+            }
+            var createdAge = await _ageService.CreateAge(age);
+            return CreatedAtAction(nameof(GetAgeById), new { id = createdAge.Id }, createdAge);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAgeById(int id)
+        {
+            var age = await _ageService.GetAgeById(id);
+            if (age == null)
+            {
+                return NotFound();
+            }
+            return Ok(age);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAge(int id, [FromBody] Age age)
+        {
+            if (age == null)
+            {
+                return BadRequest("Age cannot be null");
+            }
+            var result = await _ageService.UpdateAge(id, age);
+            if (!result)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+    }
+}
