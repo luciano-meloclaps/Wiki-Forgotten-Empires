3b97edc8bebe94c226bdb8c1073b8db818d71f3a
Luciano <meloclapsluciano@gmail.com>
2025-07-16 23:01:14 -0300

    feat: agrego DTOs en Application/Models

---
+++ Application/Models/Dto/AgeDto.cs
@@ -0,0 +1,39 @@
+﻿using Application.Models.Request.Application.Models.Request;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.Dto
+{
+    public class AgeDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+
+        //Entidad → DTO, salida
+        public static AgeDto ToDto(Age age)
+        {
+            return new AgeDto
+            {
+                Name = age.Name,
+                Description = age.Description,
+                Date = age.Date
+            };
+        }
+
+        //Request → Entidad, entrada
+        public static Age ToEntity(AgeRequest req)
+        {
+            return new Age
+            {
+                Name = req.Name,
+                Description = req.Description,
+                Date = req.Date
+            };
+        }
+    }
+}
---
+++ Application/Models/Request/AgeRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.Request
+{
+    namespace Application.Models.Request
+    {
+        public class AgeRequest
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Date { get; set; }
+        }
+    }
+}
