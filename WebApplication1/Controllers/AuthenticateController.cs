7a61365b129885c9a184a2ef718838568e60a89b
luciano-meloclaps <meloclapsluciano@gmail.com>
2025-08-31 22:18:29 -0300

    feature: JWT

--- WebApplication1/Controllers/AuthenticateController.cs
+++ WebApplication1/Controllers/AuthenticateController.cs
@@ -1,29 +1,74 @@
-using System.Runtime.InteropServices;
-using ForgottenEmpires.API.Services;
-using Microsoft.AspNetCore.Components;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
-namespace ForgottenEmpires.Controllers
+namespace ForgottenEmpire.Controllers;
+
+public class AuthenticationRequest
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthenticateController : ControllerBase
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
+
+[ApiController]
+[Route("api/authentication")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IConfiguration _config;
+
+    public AuthenticationController(IConfiguration config)
     {
-        private readonly IAuthenticateService _authenticateService;
+        _config = config;
+    }
 
-        public AuthenticateController(IAuthenticateService authenticateService)
+    [HttpPost("authenticate")]
+    public ActionResult<string> Authenticate(AuthenticationRequest request)
+    {
+        // Lógica de Validación de Usuario (Hardcodeada)
+        // En una app real, esto buscaría en la base de datos (min 27 clase 10).
+        string userRole = "";
+        if (request.Username == "admin" && request.Password == "adminpassword")
         {
-            _authenticateService = authenticateService;
+            userRole = "Admin";
+        }
+        else if (request.Username == "user" && request.Password == "userpassword")
+        {
+            userRole = "User";
+        }
+        else
+        {
+            return Unauthorized();
         }
 
-        [HttpPost]
-        public IActionResult Authenticate([FromBody] UserLogin userLogin)
+        //Si es por entidad seria esta la logica * cre* porque en la clase no se dice:
+        // llamas a repositoy para buscar el usuario, pasando username y password[DromBody],
+        // si lo encuentra devuelve un obj User, si no lo encuentra devuelve null
+
+        // Lógica de Generación de Token
+        //Prococinamos la Signature
+        var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_config["Authentication:SecretForKey"]!)); // Se usa para acceder al appsettings.json
+        var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256); //Algoritmo de cifrado
+
+        var claims = new List<Claim> //
         {
-            var token = _authenticateService.Authenticate(userLogin);
-            if (token == null)
-            {
-                return Unauthorized();
-            }
-            return Ok(new { Token = token });
+            new Claim(ClaimTypes.Name, request.Username),
+            new Claim("role", userRole)
+            // Rol ventaja en claim =>
+            // Preguntar si el usuario tiene X rol sin ir a la BD = > Más rápido y mas cash.
         }
+        ;
+
+        //Token para el usuario
+        var jwtSecurityToekn = new JwtSecurityToken( //Objeto que representa el token y datos appsettings.json
+            issuer: _config["Authentication:Issuer"],
+            audience: _config["Authentication:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(1),
+            signingCredentials: signingCredentials);
+
+        string TokenToReturn = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToekn);
+        return Ok(TokenToReturn); //Llave
     }
 }
